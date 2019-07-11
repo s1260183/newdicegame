66c60fec01afbca4f100970b83503b3af88564c0
s1260183 <s1260183@ilab2dc17.u-aizu.ac.jp>
2019-07-11 16:03:48 +0900

    Won/Lost message added

--- DiceGame1.java
+++ DiceGame1.java
@@ -8,7 +8,12 @@ public class DiceGame1{
 	int num2 = rnd.nextInt(6) + 1;
 	System.out.println("Die 1:" + num1);
 	System.out.println("Die 2:" + num2);
-	System.out.println("Total value:" + (num1 + num2));	
+	System.out.println("Total value:" + (num1 + num2));
+
+	if(num1 + num2 > 7){
+	    System.out.println("You won");
+	}
+	else System.out.println("You lost");
     }
 
 }
